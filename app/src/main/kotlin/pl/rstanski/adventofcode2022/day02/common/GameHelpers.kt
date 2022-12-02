150de2b1d7ea615bef4f99d3342f0c75fa79a934
rafal.stanski <rafal.stanski@gmail.com>
2022-12-02 10:10:40 +0100

    Refactor of day 1, part 1 solution.

---
+++ app/src/main/kotlin/pl/rstanski/adventofcode2022/day02/common/GameHelpers.kt
@@ -0,0 +1,29 @@
+package pl.rstanski.adventofcode2022.day02.common
+
+import pl.rstanski.adventofcode2022.day02.common.Shape.Paper
+import pl.rstanski.adventofcode2022.day02.common.Shape.Rock
+import pl.rstanski.adventofcode2022.day02.common.Shape.Scissors
+
+enum class Shape(val score: Int) {
+    Rock(1), Paper(2), Scissors(3)
+}
+
+enum class RoundOutcome(val score: Int) {
+    Lost(0), Draw(3), Win(6)
+}
+
+fun String.opponentChoiceAsShape(): Shape =
+    when (this) {
+        "A" -> Rock
+        "B" -> Paper
+        "C" -> Scissors
+        else -> throw IllegalArgumentException("Unknown sign for OpponentChoice: $this")
+    }
+
+fun String.ourResponseAsShape(): Shape =
+    when (this) {
+        "X" -> Rock
+        "Y" -> Paper
+        "Z" -> Scissors
+        else -> throw IllegalArgumentException("Unknown sign for Response: $this")
+    }
--- app/src/main/kotlin/pl/rstanski/adventofcode2022/day02/part1/Day02Part1.kt
+++ app/src/main/kotlin/pl/rstanski/adventofcode2022/day02/part1/Day02Part1.kt
@@ -1,12 +1,17 @@
 package pl.rstanski.adventofcode2022.day02.part1
 
-import java.lang.IllegalArgumentException
-import java.lang.IllegalStateException
 import pl.rstanski.adventofcode2022.common.Puzzle
 import pl.rstanski.adventofcode2022.common.PuzzleLoader
-import pl.rstanski.adventofcode2022.day02.part1.RoundOutcome.Draw
-import pl.rstanski.adventofcode2022.day02.part1.RoundOutcome.Lost
-import pl.rstanski.adventofcode2022.day02.part1.RoundOutcome.Win
+import pl.rstanski.adventofcode2022.day02.common.RoundOutcome
+import pl.rstanski.adventofcode2022.day02.common.RoundOutcome.Draw
+import pl.rstanski.adventofcode2022.day02.common.RoundOutcome.Lost
+import pl.rstanski.adventofcode2022.day02.common.RoundOutcome.Win
+import pl.rstanski.adventofcode2022.day02.common.Shape
+import pl.rstanski.adventofcode2022.day02.common.Shape.Paper
+import pl.rstanski.adventofcode2022.day02.common.Shape.Rock
+import pl.rstanski.adventofcode2022.day02.common.Shape.Scissors
+import pl.rstanski.adventofcode2022.day02.common.opponentChoiceAsShape
+import pl.rstanski.adventofcode2022.day02.common.ourResponseAsShape
 
 private const val PUZZLE_FILENAME = "day02/day02.txt"
 
@@ -31,20 +36,39 @@ object Day02Part1Solution {
     }
 }
 
-class RoundCalculator {
+private class RoundStrategyParser {
 
-    private val rules : Map<Pair<OpponentChoiceType, ResponseType>, RoundOutcome> = mapOf(
-        (OpponentChoiceType.Rock to ResponseType.Rock) to Draw,
-        (OpponentChoiceType.Paper to ResponseType.Paper) to Draw,
-        (OpponentChoiceType.Scissors to ResponseType.Scissors) to Draw,
-        (OpponentChoiceType.Rock to ResponseType.Paper) to Win,
-        (OpponentChoiceType.Rock to ResponseType.Scissors) to Lost,
-        (OpponentChoiceType.Paper to ResponseType.Rock) to Lost,
-        (OpponentChoiceType.Paper to ResponseType.Scissors) to Win,
-        (OpponentChoiceType.Scissors to ResponseType.Rock) to Win,
-        (OpponentChoiceType.Scissors to ResponseType.Paper) to Lost,
+    fun parse(line: String): RoundStrategy {
+        val parts: List<String> = line.split(" ")
+
+        return RoundStrategy(
+            opponentChoice = parts[0].opponentChoiceAsShape(),
+            response = parts[1].ourResponseAsShape()
+        )
+    }
+}
+
+private data class RoundStrategy(
+    val opponentChoice: Shape,
+    val response: Shape
+)
+
+private class RoundCalculator {
+
+    private val rules: Map<Pair<Shape, Shape>, RoundOutcome> = mapOf(
+        (Rock to Rock) to Draw,
+        (Paper to Paper) to Draw,
+        (Scissors to Scissors) to Draw,
+
+        (Rock to Paper) to Win,
+        (Rock to Scissors) to Lost,
+
+        (Paper to Rock) to Lost,
+        (Paper to Scissors) to Win,
+
+        (Scissors to Rock) to Win,
+        (Scissors to Paper) to Lost,
     )
-
     fun calculate(roundStrategy: RoundStrategy): Int {
         val round = roundStrategy.opponentChoice to roundStrategy.response
         val roundOutcome = rules[round] ?: throw IllegalStateException("Unknown round: $round")
@@ -53,47 +77,3 @@ class RoundCalculator {
     }
 }
 
-class RoundStrategyParser {
-
-    fun parse(line: String): RoundStrategy {
-        val parts: List<String> = line.split(" ")
-
-        return RoundStrategy(
-            opponentChoice = OpponentChoiceType.toType(parts[0]),
-            response = ResponseType.toType(parts[1])
-        )
-    }
-}
-
-data class RoundStrategy(
-    val opponentChoice: OpponentChoiceType,
-    val response: ResponseType
-)
-
-enum class OpponentChoiceType(private val sign: String) {
-    Rock("A"), Paper("B"), Scissors("C");
-
-    companion object {
-        fun toType(sign: String): OpponentChoiceType {
-            return values()
-                .find { it.sign == sign }
-                ?: throw IllegalArgumentException("Unknown sign for OpponentChoice: $sign")
-        }
-    }
-}
-
-enum class ResponseType(private val sign: String, val score: Int) {
-    Rock("X", 1), Paper("Y", 2), Scissors("Z", 3);
-
-    companion object {
-        fun toType(sign: String): ResponseType {
-            return values()
-                .find { it.sign == sign }
-                ?: throw IllegalArgumentException("Unknown sign for Response: $sign")
-        }
-    }
-}
-
-enum class RoundOutcome(val score: Int) {
-    Lost(0), Draw(3), Win(6)
-}
