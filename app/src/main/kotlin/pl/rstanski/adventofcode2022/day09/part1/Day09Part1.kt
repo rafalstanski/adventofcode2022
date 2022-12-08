a225fc47d16038b3d3ba091505b13ec280c07203
rafal.stanski <rafal.stanski@gmail.com>
2022-12-08 13:02:27 +0100

    Prepared for day 09.

---
+++ app/src/main/kotlin/pl/rstanski/adventofcode2022/day09/part1/Day09Part1.kt
@@ -0,0 +1,21 @@
+package pl.rstanski.adventofcode2022.day09.part1
+
+import pl.rstanski.adventofcode2022.common.Puzzle
+import pl.rstanski.adventofcode2022.common.PuzzleLoader
+
+private const val PUZZLE_FILENAME = "day09.txt"
+
+fun main() {
+    val puzzle: Puzzle = PuzzleLoader.load(PUZZLE_FILENAME)
+
+    val result = Day09Part1Solution.solve(puzzle)
+
+    println(result)
+}
+
+object Day09Part1Solution {
+
+    fun solve(puzzle: Puzzle): Any {
+        TODO()
+    }
+}
