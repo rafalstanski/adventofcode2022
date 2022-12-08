48fc9de2afcf9f1e0884aab1a9023f67088eeab2
rafal.stanski <rafal.stanski@gmail.com>
2022-12-08 14:11:01 +0100

    Refactor of day 08: fixed wrong border usage.

--- app/src/main/kotlin/pl/rstanski/adventofcode2022/day08/common/Forrest.kt
+++ app/src/main/kotlin/pl/rstanski/adventofcode2022/day08/common/Forrest.kt
@@ -9,7 +9,7 @@ class Forrest(val xSize: Int, val ySize: Int) {
     }
 
     fun getTree(x: Int, y: Int): Tree =
-        Tree(x, y, trees[x][y], this)
+        Tree(x, y, trees[x][y])
 
     fun allTrees(): Sequence<Tree> =
         sequence {
@@ -22,43 +22,47 @@ class Forrest(val xSize: Int, val ySize: Int) {
 
     fun goLeftFrom(tree: Tree): Sequence<Tree> =
         when {
-            tree.isFromBoarder() -> emptySequence()
+            isLeftBoarderTree(tree) -> emptySequence()
             else -> sequence {
-                (tree.x - 1 downTo 0).forEach { x ->
-                    yield(getTree(x, tree.y))
-                }
+                (tree.x - 1 downTo 0).forEach { x -> yield(getTree(x, tree.y)) }
             }
         }
 
     fun goRightFrom(tree: Tree): Sequence<Tree> =
         when {
-            tree.isFromBoarder() -> emptySequence()
+            isRightBoarderTree(tree) -> emptySequence()
             else -> sequence {
-                (tree.x + 1 until xSize).forEach { x ->
-                    yield(getTree(x, tree.y))
-                }
+                (tree.x + 1 until xSize).forEach { x -> yield(getTree(x, tree.y)) }
             }
         }
 
     fun goTopFrom(tree: Tree): Sequence<Tree> =
         when {
-            tree.isFromBoarder() -> emptySequence()
+            isTopBoarderTree(tree) -> emptySequence()
             else -> sequence {
-                (tree.y + 1 until ySize).forEach { y ->
-                    yield(getTree(tree.x, y))
-                }
+                (tree.y + 1 until ySize).forEach { y -> yield(getTree(tree.x, y)) }
             }
         }
 
     fun goBottomFrom(tree: Tree): Sequence<Tree> =
         when {
-            tree.isFromBoarder() -> emptySequence()
+            isBottomBoarderTree(tree) -> emptySequence()
             else -> sequence {
-                (tree.y - 1 downTo 0).forEach { y ->
-                    yield(getTree(tree.x, y))
-                }
+                (tree.y - 1 downTo 0).forEach { y -> yield(getTree(tree.x, y)) }
             }
         }
+
+    private fun isLeftBoarderTree(tree: Tree): Boolean =
+        tree.x == 0
+
+    private fun isRightBoarderTree(tree: Tree): Boolean =
+        tree.x == xSize - 1
+
+    private fun isTopBoarderTree(tree: Tree): Boolean =
+        tree.y == ySize - 1
+
+    private fun isBottomBoarderTree(tree: Tree): Boolean =
+        tree.y == 0
 }
 
 fun Sequence<Tree>.findHigherOrEqualsThen(tree: Tree): Tree? =
--- app/src/main/kotlin/pl/rstanski/adventofcode2022/day08/common/Tree.kt
+++ app/src/main/kotlin/pl/rstanski/adventofcode2022/day08/common/Tree.kt
@@ -3,9 +3,5 @@ package pl.rstanski.adventofcode2022.day08.common
 data class Tree(
     val x: Int,
     val y: Int,
-    val height: Int,
-    private val forest: Forrest
-) {
-    fun isFromBoarder(): Boolean =
-        x == 0 || y == 0 || x == forest.xSize - 1 || y == forest.ySize
-}
+    val height: Int
+)
