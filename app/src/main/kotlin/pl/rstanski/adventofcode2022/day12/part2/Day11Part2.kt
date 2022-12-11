085d2e71f028d825c244e63434781cfcd9b27856
rafal.stanski <rafal.stanski@gmail.com>
2022-12-11 20:58:11 +0100

    Prepared for day 12.

---
+++ app/src/main/kotlin/pl/rstanski/adventofcode2022/day12/part2/Day11Part2.kt
@@ -0,0 +1,22 @@
+package pl.rstanski.adventofcode2022.day12.part2
+
+import pl.rstanski.adventofcode2022.common.Puzzle
+import pl.rstanski.adventofcode2022.common.PuzzleLoader
+
+private const val PUZZLE_FILENAME = "day12.txt"
+
+fun main() {
+    val puzzle: Puzzle = PuzzleLoader.load(PUZZLE_FILENAME)
+
+    val result = Day12Part2Solution.solve(puzzle)
+
+    println(result)
+}
+
+object Day12Part2Solution {
+
+    fun solve(puzzle: Puzzle): Any {
+
+        return TODO()
+    }
+}
---
+++ app/src/test/kotlin/pl/rstanski/adventofcode2022/Day12Test.kt
@@ -0,0 +1,31 @@
+package pl.rstanski.adventofcode2022
+
+import kotlin.test.assertEquals
+import org.junit.Test
+import pl.rstanski.adventofcode2022.day12.part1.Day12Part1Solution
+import pl.rstanski.adventofcode2022.day12.part2.Day12Part2Solution
+
+class Day12Test : BaseTest() {
+
+    private val part1Solution = Day12Part1Solution
+    private val part2Solution = Day12Part2Solution
+
+    //given
+    private val puzzle = puzzleFrom("day12sample.txt")
+
+    @Test
+    fun shouldFindCorrectSolutionOfPart1ForSamplePuzzle() {
+        //when
+        val solution = part1Solution.solve(puzzle)
+        //then
+        assertEquals(expected = 0, actual = solution)
+    }
+
+    @Test
+    fun shouldFindCorrectSolutionOfPart2ForSamplePuzzle() {
+        //when
+        val solution = part2Solution.solve(puzzle)
+        //then
+        assertEquals(expected = 0, actual = solution)
+    }
+}
