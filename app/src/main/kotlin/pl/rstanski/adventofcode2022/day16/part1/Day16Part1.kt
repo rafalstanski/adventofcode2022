d176e9a9f02c4d632f820534f97b3a8f71090e32
rafal.stanski <rafal.stanski@gmail.com>
2022-12-15 21:45:02 +0100

    Prepared for day 16.

---
+++ app/src/main/kotlin/pl/rstanski/adventofcode2022/day16/part1/Day16Part1.kt
@@ -0,0 +1,21 @@
+package pl.rstanski.adventofcode2022.day16.part1
+
+import pl.rstanski.adventofcode2022.common.Puzzle
+import pl.rstanski.adventofcode2022.common.PuzzleLoader.load
+
+fun main() {
+    check(solvePart1(load("day16sample.txt")) == 0)
+
+    println("solution: " + solvePart1(load("day16.txt")))
+}
+
+fun solvePart1(puzzle: Puzzle): Any {
+    puzzle.lines.
+
+    return 1
+}
+
+fun parse(line: String) {
+
+}
+
---
+++ app/src/main/kotlin/pl/rstanski/adventofcode2022/day16/part2/Day16Part2.kt
@@ -0,0 +1,15 @@
+package pl.rstanski.adventofcode2022.day16.part2
+
+import pl.rstanski.adventofcode2022.common.Puzzle
+import pl.rstanski.adventofcode2022.common.PuzzleLoader
+
+fun main() {
+    check(solvePart2(PuzzleLoader.load("day16sample.txt")) == 0)
+
+    println("solution: " + solvePart2(PuzzleLoader.load("day16.txt")))
+}
+
+fun solvePart2(puzzle: Puzzle): Any {
+
+    return 0
+}
