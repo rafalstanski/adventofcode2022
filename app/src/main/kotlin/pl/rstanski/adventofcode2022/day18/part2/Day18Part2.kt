12e6f999648470120f94d0e9303ea3fdbf28ecd1
rafal.stanski <rafal.stanski@gmail.com>
2022-12-18 08:32:38 +0100

    (raw) Day 18 solution for part 2.

--- app/src/main/kotlin/pl/rstanski/adventofcode2022/day18/part2/Day18Part2.kt
+++ app/src/main/kotlin/pl/rstanski/adventofcode2022/day18/part2/Day18Part2.kt
@@ -1,5 +1,6 @@
 package pl.rstanski.adventofcode2022.day18.part2
 
+import java.util.LinkedList
 import kotlin.math.absoluteValue
 import pl.rstanski.adventofcode2022.common.Puzzle
 import pl.rstanski.adventofcode2022.common.PuzzleLoader.load
@@ -24,29 +25,52 @@ private fun solvePart2(puzzle: Puzzle): Any {
     val maxY = positions.maxOf { it.y }
     val maxZ = positions.maxOf { it.z }
 
-    println("$minX, $minY, $minZ")
-    println("$maxX, $maxY, $maxZ")
     val min = Position(minX, minY, minZ)
     val max = Position(maxX, maxY, maxZ)
 
-    val airpockets = (minX + 1..maxX - 1).map { x ->
-        (minY + 1..maxY - 1).map { y ->
-            (minZ + 1..maxZ - 1).map { z ->
-                AirCube(Position(x, y, z), min, max)
-            }
+    println("min: $min, max: $max")
+
+    val start = min - Position(1, 1, 1)
+    val visited = mutableSetOf<Position>()
+    val reachableAir = mutableSetOf<Position>()
+    val toCheckQueue = LinkedList<Position>()
+
+    toCheckQueue.add(start)
+
+    while (toCheckQueue.isNotEmpty()) {
+        val toCheck = toCheckQueue.poll()
+
+        if (toCheck in visited) continue
+        visited.add(toCheck)
+
+        if (toCheck in positions) continue
+
+        reachableAir += toCheck
+
+        toCheck.neigbours().filter {
+            it !in positions
+                    && it.x >= min.x - 1 && it.x <= max.x + 1
+                    && it.y >= min.y - 1 && it.y <= max.y + 1
+                    && it.z >= min.z - 1 && it.z <= max.z + 1
+        }.forEach {
+            toCheckQueue.add(it)
         }
-    }.flatten()
-        .flatten()
-        .filter { it.position !in positions }
-        .filter { it.isAirPocket(positions) }
+    }
 
-    val airPocketsPositions = airpockets.map { it.position }
-    println(airPocketsPositions)
+    val allAir =
+        (minX..maxX).map { x ->
+            (minY..maxY).map { y ->
+                (minZ..maxZ).map { z ->
+                    Position(x, y, z)
+                }
+            }
+        }.flatten().flatten().filter { it !in positions }
 
-    val positionsWithAirPockets = positions + airPocketsPositions
+    val airPockets = allAir - reachableAir
+    println(airPockets)
 
     val cubes = positions.map { Cube(it) }
-    return cubes.map { it.countUncoveredSides(positionsWithAirPockets) }.sumOf { it }
+    return cubes.map { it.countUncoveredSides(positions + airPockets) }.sumOf { it }
 }
 
 data class Cube(val position: Position) {
@@ -81,28 +105,30 @@ data class Cube(val position: Position) {
 
 }
 
-data class AirCube(val position: Position, val min: Position, val max: Position) {
-
-    fun isAirPocket(positions: List<Position>): Boolean {
-        val of = setOf(
-            (position.x + 1..max.x).map { position.copy(x = it) }.find { it in positions },
-            (min.x..position.x - 1).map { position.copy(x = it) }.find { it in positions },
-            (position.y + 1..max.y).map { position.copy(y = it) }.find { it in positions },
-            (min.y..position.y - 1).map { position.copy(y = it) }.find { it in positions },
-            (position.z + 1..max.z).map { position.copy(z = it) }.find { it in positions },
-            (min.z..position.z - 1).map { position.copy(z = it) }.find { it in positions }
-        )
-        if (position == Position(2, 2, 5)) {
-            println("checking airpocket: $this: $of")
-            println(position !in positions)
-        }
-        return of.toList().size == 6
-    }
-}
-
 fun parse(line: String): Position {
     val parts = line.split(",").toInts()
     return Position(parts[0], parts[1], parts[2])
 }
 
-data class Position(val x: Int, val y: Int, val z: Int)
+data class Position(val x: Int, val y: Int, val z: Int) {
+    fun left() = this.copy(x = x - 1)
+    fun right() = this.copy(x = x + 1)
+    fun bottom() = this.copy(y = y - 1)
+    fun top() = this.copy(y = y + 1)
+    fun front() = this.copy(z = z - 1)
+    fun back() = this.copy(z = z + 1)
+
+    fun neigbours(): List<Position> {
+        return listOf(
+            left(),
+            right(),
+            top(),
+            bottom(),
+            back(),
+            front()
+        )
+    }
+
+    operator fun minus(otherPosition: Position): Position =
+        Position(x - otherPosition.x, y - otherPosition.y, z - otherPosition.z)
+}
