8a57682c5103b4deac85376180abd4b8e480d218
rafal.stanski <rafal.stanski@gmail.com>
2022-12-20 11:29:24 +0100

    (raw) Day 20 solution for part 1 and part 2.

--- app/src/main/kotlin/pl/rstanski/adventofcode2022/day20/part1/Day20Part1.kt
+++ app/src/main/kotlin/pl/rstanski/adventofcode2022/day20/part1/Day20Part1.kt
@@ -2,16 +2,48 @@ package pl.rstanski.adventofcode2022.day20.part1
 
 import pl.rstanski.adventofcode2022.common.Puzzle
 import pl.rstanski.adventofcode2022.common.PuzzleLoader.load
+import pl.rstanski.adventofcode2022.common.toInts
 
 fun main() {
     val testSolution = solvePart1(load("day20sample.txt"))
     println("test solution: $testSolution")
-    check(testSolution == 0)
+    check(testSolution == 3)
 
     val solution = solvePart1(load("day20.txt"))
     println("solution: $solution")
 }
 
 private fun solvePart1(puzzle: Puzzle): Any {
-    TODO()
+    val numbers = puzzle.lines.toInts()
+
+    val encryptedFile = EncryptedFile(numbers)
+    encryptedFile.mix()
+    return encryptedFile.groveCoordinatesSum()
 }
+
+class EncryptedFile(originalNumbers: List<Int>) {
+    private val original = originalNumbers
+        .mapIndexed { index: Int, i: Int ->  NumberNode(index, i) }
+        .toMutableList()
+    private val mixed = original.toMutableList()
+
+    fun mix() {
+        original.forEach { number: NumberNode ->
+            val currentIndex = mixed.indexOf(number)
+            val newIndex = (currentIndex + number.value).mod(mixed.size - 1)
+
+            mixed.removeAt(currentIndex)
+            mixed.add(newIndex, number)
+        }
+    }
+
+    fun groveCoordinatesSum(): Int {
+        val indexOfZero = mixed.indexOfFirst { node -> node.value == 0 }
+
+        return listOf(1000, 2000, 3000)
+            .map { (indexOfZero + it) % mixed.size }
+            .sumOf { mixed[it].value }
+    }
+}
+
+data class NumberNode(val index: Int, val value: Int)
--- app/src/main/kotlin/pl/rstanski/adventofcode2022/day20/part2/Day20Part2.kt
+++ app/src/main/kotlin/pl/rstanski/adventofcode2022/day20/part2/Day20Part2.kt
@@ -2,16 +2,50 @@ package pl.rstanski.adventofcode2022.day20.part2
 
 import pl.rstanski.adventofcode2022.common.Puzzle
 import pl.rstanski.adventofcode2022.common.PuzzleLoader.load
+import pl.rstanski.adventofcode2022.common.toLongs
 
 fun main() {
     val testSolution = solvePart2(load("day20sample.txt"))
     println("test solution: $testSolution")
-    check(testSolution == 0)
+    check(testSolution == 1623178306L)
 
     val solution = solvePart2(load("day20.txt"))
     println("solution: $solution")
 }
 
 private fun solvePart2(puzzle: Puzzle): Any {
-    TODO()
+    val numbers = puzzle.lines.toLongs()
+
+    val encryptedFile = EncryptedFile(numbers)
+    repeat(10) {
+        encryptedFile.mix()
+    }
+    return encryptedFile.groveCoordinatesSum()
 }
+
+class EncryptedFile(originalNumbers: List<Long>) {
+    private val original = originalNumbers
+        .mapIndexed { index: Int, i: Long ->  NumberNode(index, i * 811589153) }
+        .toMutableList()
+    private val mixed = original.toMutableList()
+
+    fun mix() {
+        original.forEach { number: NumberNode ->
+            val currentIndex = mixed.indexOf(number)
+            val newIndex = (currentIndex + number.value).mod(mixed.size - 1)
+
+            mixed.removeAt(currentIndex)
+            mixed.add(newIndex, number)
+        }
+    }
+
+    fun groveCoordinatesSum(): Long {
+        val indexOfZero = mixed.indexOfFirst { node -> node.value == 0L }
+
+        return listOf(1000, 2000, 3000)
+            .map { (indexOfZero + it) % mixed.size }
+            .sumOf { mixed[it].value }
+    }
+}
+
+data class NumberNode(val index: Int, val value: Long)
