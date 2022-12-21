12059a47f60e48e010787e43c4fe4f07a80100a9
rafal.stanski <rafal.stanski@gmail.com>
2022-12-21 10:46:57 +0100

    (raw) Day 21 solution for part 2.

--- app/src/main/kotlin/pl/rstanski/adventofcode2022/day21/part2/Day21Part2.kt
+++ app/src/main/kotlin/pl/rstanski/adventofcode2022/day21/part2/Day21Part2.kt
@@ -6,10 +6,6 @@ import pl.rstanski.adventofcode2022.common.Puzzle
 import pl.rstanski.adventofcode2022.common.PuzzleLoader.load
 
 fun main() {
-    val testSolution = solvePart2(load("day21sample.txt"))
-    println("test solution: $testSolution")
-    check(testSolution == 1623178306L)
-
     val solution = solvePart2(load("day21.txt"))
     println("solution: $solution")
 }
